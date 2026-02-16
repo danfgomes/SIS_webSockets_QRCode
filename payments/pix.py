2d814b2bb69ded2f8e79e1963903052540c5c3d8
daniel <dg1638382@gmail.com>
2026-02-16 16:15:52 -0300

    implement websocket

--- payments/pix.py
+++ payments/pix.py
@@ -6,22 +6,20 @@ class Pix:
         pass
 
     def create_payment(self):
+        # criar pagamendo da instituicao financeira
 
         bank_payment_id = str(uuid.uuid4())
 
         hash_payment = f"hash_payment_{bank_payment_id}"
 
+        #qr code 
         img = qrcode.make(hash_payment)
 
         
         qr_code = f"qr_code_payment_{bank_payment_id}"   # <-- definiu aqui
         img.save(f"static/img/{qr_code}.png")
-
-
-    
-    
-    
+        
         return {
             "bank_payment_id": bank_payment_id,
-            "qr_code": qr_code
-        }
+            "qr_code_path": f"qr_code_payment_{bank_payment_id}"}
+        
